e748122d827dcff4f110842fd8654e09d8b169a1
astainy1 <astainyharris1@gmail.com>
2025-08-11 17:31:25 +0000

    update 2

--- CMS/my-strapi-backend/config/middlewares.ts
+++ CMS/my-strapi-backend/config/middlewares.ts
@@ -4,10 +4,8 @@ module.exports = [
   {
     name: "strapi::cors",
     config: {
-      origin: [
-        "http://localhost:5173",
-        "https://gabriel-3191997.github.io/Raphaella/",
-      ], // frontend URL here
+      origin: "https://gabriel-3191997.github.io/Raphaella",
+      // frontend URL here
       // origin: [process.env.FRONTEND_URL || "http://localhost:5173"], // frontend URL here
       methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     },
